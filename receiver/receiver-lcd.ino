9b4aa72e9333148f8498f0b580105de7ea54e4fb
Lifaet Hossain Zim <lifaethossain@gmail.com>
2023-08-14 17:43:00 +0600

    minor fix

--- receiver/receiver-lcd.ino
+++ receiver/receiver-lcd.ino
@@ -15,14 +15,20 @@ void setup() {
 
 void loop() {
   unsigned long duration = pulseIn(8, HIGH);
+
   if (duration > 10000 && duration < 18000)
   {
     lcd.setCursor(0, 0);
-    lcd.print("Received:0");
+    lcd.print("0 Received");
   }
   else if (duration > 20000 && duration < 28000)
   {
     lcd.setCursor(0, 0);
-    lcd.print("Received:1");
+    lcd.print("1 Received");
+  }
+  else if (duration > 30000 && duration < 38000)
+  {
+    lcd.setCursor(0, 0);
+    lcd.print("End Message");
   }
 }
--- sender/sender.ino
+++ sender/sender.ino
@@ -16,15 +16,23 @@ void loop()
   if (sendBit == '1')
   {
     digitalWrite(8, HIGH);
-    delay(20);
-    digitalWrite(8, LOW);
-    Serial.println("'1' Send");
-  }
-  else if (sendBit == '0')
-  {
-    digitalWrite(8, HIGH);
     delay(10);
     digitalWrite(8, LOW);
     Serial.println("'0' Send");
   }
+  else if (sendBit == '0')
+  {
+    digitalWrite(8, HIGH);
+    delay(20);
+    digitalWrite(8, LOW);
+    Serial.println("'1' Send");
+  }
+  else if (sendBit == '2')
+  {
+    digitalWrite(8, HIGH);
+    delay(30);
+    digitalWrite(8, LOW);
+    Serial.println("End Message");
+  }
+  delay(50);
 }
